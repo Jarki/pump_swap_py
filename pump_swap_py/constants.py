5ccf6f596efaaff01a6a8bd2fe44191e8acdb6f1
AL THE BOT FATHER <98119917+AL-THE-BOT-FATHER@users.noreply.github.com>
2025-03-23 18:53:22 +0100

    Update constants.py

--- pump_swap_py/constants.py
+++ pump_swap_py/constants.py
@@ -3,13 +3,9 @@ from solders.pubkey import Pubkey #type: ignore
 GLOBAL_CONFIG = Pubkey.from_string("ADyA8hdefvWN2dbGGWFotbzWxrAvLW83WG6QCVXvJKqw")
 SYSTEM_PROGRAM = Pubkey.from_string("11111111111111111111111111111111")
 ASSOCIATED_TOKEN_PROGRAM = Pubkey.from_string("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL")
+PROTOCOL_FEE_RECIPIENT = Pubkey.from_string("62qc2CNXwrYqQScmEdiZFFAnJR262PxWEuNQtxfafNgV")
 EVENT_AUTH = Pubkey.from_string("GS4CU59F31iL7aR2Q8zVS8DRrcRnXX1yjQ66TqNVQnaR")
 PF_AMM = Pubkey.from_string("pAMMBay6oceH9fJKBRHGP5D4bD4sWpmSwMn52FMfXEA")
-
-PROTOCOL_FEE_RECIPIENTS = [Pubkey.from_string("62qc2CNXwrYqQScmEdiZFFAnJR262PxWEuNQtxfafNgV"), 
-                           Pubkey.from_string("G5UZAVbAf46s7cKWoyKu8kYTip9DGTpbLZ2qa9Aq69dP"), 
-                           Pubkey.from_string("7hTckgnGnLQR6sdH7YkqFTAA7VwTfYFaZ6EhEsU3saCX"), 
-                           Pubkey.from_string("AVmoTthdrX6tKt4nDjco2D775W2YK3sDhxPcMmzUAmTY")]
 
 TOKEN_PROGRAM_ID = Pubkey.from_string("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
 ACCOUNT_SPACE = 165
